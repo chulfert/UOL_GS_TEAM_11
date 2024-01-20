42dc6cfe7d39775c1e0ed003e8db88b39547b1a2
Eloy Gonzalez Gesto <eloy.gonzalezgesto@hotmail.com>
2024-01-20 18:38:05 +0100

    Functional menu scene

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeSceneOrQuit : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     public void StartGame()
     {
