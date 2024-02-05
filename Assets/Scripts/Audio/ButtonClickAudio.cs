e4e03e307248253dcc01a4cef94f8526eb52fdeb
ZanderDUP <97292254+ZanderDUP@users.noreply.github.com>
2024-02-05 15:34:02 +0200

    Update to click scrtipt

---
+++ Assets/Scripts/Audio/ButtonClickAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonClickAudio : MonoBehaviour, IPointerClickHandler
+{
+    public AudioClip hoverSound;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = hoverSound;
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        audioSource.volume = 0.5f;
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+}
