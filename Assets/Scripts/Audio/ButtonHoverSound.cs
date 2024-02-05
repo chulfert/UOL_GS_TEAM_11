7c352b75de49c5a47958cf6de1f521012d047748
ZanderDUP <97292254+ZanderDUP@users.noreply.github.com>
2024-02-05 12:53:20 +0200

    Added Audio Assets and Scripts

---
+++ Assets/Scripts/Audio/ButtonHoverSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonHoverSound : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip hoverSound;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = hoverSound;
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+}
