10cfc23a3c4c6e745a477fe2da8875552190164e
ZanderDUP <97292254+ZanderDUP@users.noreply.github.com>
2024-02-06 07:58:19 +0200

    Fixed button clicked audio

---
+++ Assets/Scripts/Audio/ButtonAudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAudioManager : MonoBehaviour
+{
+    public AudioClip clickSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Ensure the audio source persists between scenes
+        GameObject audioManager = GameObject.Find("AudioManager");
+
+        // Create new audio manager on play
+        if (audioManager == null)
+        {
+            audioManager = new GameObject("AudioManager");
+            DontDestroyOnLoad(audioManager);
+        }
+
+        // Set up audio source
+        audioSource = audioManager.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = audioManager.AddComponent<AudioSource>();
+            
+            // Audio Source properties
+            audioSource.loop = true;
+            audioSource.volume = 2.0f;
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        // Play the click sound when a button is clicked
+        audioSource.PlayOneShot(clickSound);
+    }
+}
--- Assets/Scripts/Audio/ButtonClickAudio.cs
+++ Assets/Scripts/Audio/ButtonClickAudio.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-
 public class ButtonClickAudio : MonoBehaviour, IPointerClickHandler
 {
-    public AudioClip hoverSound;
-
-    private AudioSource audioSource;
+    public ButtonAudioManager buttonAudioManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.clip = hoverSound;
-        audioSource.loop = false;
-        audioSource.playOnAwake = false;
-        audioSource.volume = 0.5f;
+
 
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        audioSource.PlayOneShot(hoverSound);
+        // Get 'ButtonAudioManager' script
+        buttonAudioManager = FindObjectOfType<ButtonAudioManager>();
+
+        // Call function
+        buttonAudioManager.PlayClickSound();
+
     }
 
 }
