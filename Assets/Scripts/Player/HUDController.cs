b0a0971f0f50cbcf1028a99058897bf2d9092008
George Fleming <67323735+Grogfl@users.noreply.github.com>
2024-02-09 16:42:19 +0000

    hud basics

---
+++ Assets/Scripts/Player/HUDController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HUDController : MonoBehaviour
+{
+    public Slider HPbar;
+    public void SetMaxHP(float HP)
+    {
+        HPbar.maxValue = HP;
+        HPbar.value = HP;
+    }
+    public void SetHP(float HP)
+    {
+        HPbar.value = HP;
+    }
+}
